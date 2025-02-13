a935661f2c69193f990b2cdfc85680b04a50ae26
Renato Melo <renato.bmelo@outlook.com>
2025-02-12 22:47:19 -0300

    consumidor

--- src/main/java/org/example/Consumidor.java
+++ src/main/java/org/example/Consumidor.java
@@ -1,4 +1,27 @@
-package org.example;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 
 public class Consumidor {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Consumidor");
+
+        String NOME_FILA = "filaHelloWorld";
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        Connection conexao = connectionFactory.newConnection();
+
+        Channel canal = conexao.createChannel();
+        canal.queueDeclare(NOME_FILA, false, false, false, null);
+
+        DeliverCallback callback = (consumerTag, delivery) -> {
+            String mensagem = new String(delivery.getBody());
+            System.out.println("Recebi a mensagem: " + mensagem);
+        };
+
+        canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {});
+    }
+
 }
